b1b9b2506d0943370f6ac55cfd42ae462270d3eb
Alex Rocha <alex.rochas@yahoo.com.br>
2018-11-26 16:23:56 -0200

    fix: commentary place

--- i3wm_cheat_sheet.rb
+++ i3wm_cheat_sheet.rb
@@ -1,10 +1,10 @@
 
-File.new('config')
-  .select{ |line| line.include? 'bind' and not line.start_with? '#' }
-  .map{ |line| 
-    /bind(?:code|sym)\s(?<bind>\S*)\s(?<action>[^#]*)(?<commentary>#.*)?$/ =~ line
+File.new('config').read.scan(/(?<commentary>#.*\n)?^bind(?:code|sym)\s(?<bind>\S*)\s(?<action>.*)$/)
+  .map { |m| 
+    commentary, bind, action = m
     action.delete! "\n" if action
     action.strip! if action
+    commentary.delete! "\n" if commentary
     {
       bind: bind,
       action: action,
@@ -13,6 +13,6 @@ File.new('config')
   }
   .reject { |line| line[:bind].nil? }
   .reject { |line| line.empty? }
-  .each { |line| 
+  .each { |line|
     printf("%-30s %-30s %s\n", line[:bind],line[:action],line[:commentary])
   }
